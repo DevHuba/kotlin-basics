4cd54b4d1dc41e976cb934a89feb56acf53183b7
DevHuba <zzzz@dot.ee>
2022-03-02 10:27:37 +0200

    add if statement

--- app/src/main/java/com/devhuba/kotlinbasics/Check (if,when).kt
+++ app/src/main/java/com/devhuba/kotlinbasics/Check (if,when).kt
@@ -1,6 +1,17 @@
 package com.devhuba.kotlinbasics
 
 fun main() {
+    val heightPerson1 = 170
+    val heightPerson2 = 189
+
+    if(heightPerson1 > heightPerson2){
+        println("use raw force")
+    }else if(heightPerson2 == heightPerson1){
+        println("use technique")
+    }else{
+        println("use both")
+    }
+    
     val age = 10
     when {
         age in 16..17 -> {
@@ -13,7 +24,7 @@ fun main() {
             println("You may drink !")
         }
         else -> {
-            println("You`r too young for that shit !!")
+            println("You`r too young for all of that !!")
         }
     }
 }
