b2b667693e0a255883145e3c54b0375d24688bbf
DevHuba <devhuba@gmail.com>
2022-03-04 20:37:23 +0200

    add all basic for loop implementations

--- app/src/main/java/com/devhuba/kotlinbasics/EX/Ex 3 ( loops ).kt
+++ app/src/main/java/com/devhuba/kotlinbasics/EX/Ex 3 ( loops ).kt
@@ -4,9 +4,20 @@ package com.devhuba.kotlinbasics.EX
 // from 100 to 0
 
 fun main() {
-    var number = 100
+    var number = 10
     while (number >= 0) {
-        println(number)
+        if (number == 0) {
+            print("$number\n")
+        } else {
+            print("$number, ")
+        }
         number -= 2
+    }
+    for (iter in 10 downTo 1) {
+        if (iter == 1) {
+            print("iter -> ${iter}\n")
+        } else {
+            print("iter -> ${iter}, ")
+        }
     }
 }
--- app/src/main/java/com/devhuba/kotlinbasics/Loops.kt
+++ app/src/main/java/com/devhuba/kotlinbasics/Loops.kt
@@ -1,12 +1,48 @@
 package com.devhuba.kotlinbasics
 
 fun main() {
+
+    //While example 1
     var i = 1
-    while(i <= 10) {
-        println("Loop with i $i")
+    while (i <= 5) {
+        if (i == 5) {
+            print("While loop $i \n")
+        } else {
+            print("While loop $i ")
+        }
         i++
     }
 
-    //Do while loop
+    //While example 2
+    var feltTemp = "cold"
+    var roomTemp = 10
+    while (feltTemp == "cold") {
+        roomTemp++
+        if (roomTemp >= 20) {
+            feltTemp = "comfy"
+            println("it`s comfy now")
+        }
+    }
 
+    //For loops
+    for (iter in 1..3) {
+        print("a -> ${iter} || ")
+    }
+
+    for (iter2 in 1 until 3) {//Same as -
+        // for(iter2 in 1.until(3))
+        print("b -> ${iter2} // ")
+    }
+
+    for (iter3 in 6 downTo 0 step 2) { //Same as -
+        // for(iter3 in 6.downTo(1).step(2)
+        print("c -> ${iter3} ")
+    }
+
+    //Do while loop
+    var x = 15
+    do {
+        print("x -> $x ")
+        x--
+    } while (x > 10)
 }
