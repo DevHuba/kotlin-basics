e35f80f52663b64679a48b30117abdfe83a4247d
DevHuba <devhuba@gmail.com>
2022-03-07 22:55:46 +0200

    start with arrays closer

---
+++ app/src/main/java/com/devhuba/kotlinbasics/Arrays.kt
@@ -0,0 +1,7 @@
+package com.devhuba.kotlinbasics
+
+fun main() {
+//    val numbers:IntArray = intArrayOf(1,57,8,94,53)
+//    val numbers = intArrayOf(1, 57, 8, 94, 53)
+    val numbers = arrayOf(1, 57, 8, 94, 53)
+}
