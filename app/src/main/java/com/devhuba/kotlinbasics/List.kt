6fb7283d169b00de9cef0b1649cd04c216e846e8
DevHuba <devhuba@gmail.com>
2022-03-09 23:14:47 +0200

    list collection examples

--- app/src/main/java/com/devhuba/kotlinbasics/List.kt
+++ app/src/main/java/com/devhuba/kotlinbasics/List.kt
@@ -6,15 +6,43 @@ fun main() {
 //    print(anyTypes)
 //    println(anyTypes.size)
 //    print(months[2])
-    
+
     for (month in months) {
-        print("${month}, ")
+        println(month)
     }
 
-    //Add array into another array
+    //Add array into another list
+    //Make month array mutable
     val additionalMonths = months.toMutableList()
-    val newMonths = arrayOf("April", "May", "June")
+    //Create array with items for add
+    val newMonths = arrayOf(
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    )
+    //Add new items into mutable list
     additionalMonths.addAll(newMonths)
-    print("$additionalMonths")
-    
+//    print("$additionalMonths")
+    //Change special month in list
+    additionalMonths[4] = "Missing month"
+//    println(additionalMonths)
+
+    val days =
+        mutableListOf<String>("Month", "Tue", "Wed")
+    days.add("Thu")
+    days.removeAt(0)
+    println(days)
+    //Remove special items group from list
+    val removeFromList =
+        mutableListOf<String>("Month", "Wed")
+    days.removeAll(removeFromList)
+    //Remove all from list
+//    days.removeAll(days)
+    println(days)
 }
